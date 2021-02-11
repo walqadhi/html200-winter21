c0ae659ec33c0a565c073d060fa355dce4283878
walqadhi <wahby.alqadhi@wsu.edu>
2021-02-10 20:47:43 -0800

    n

---
+++ Lesson 3/exercise-one.js
@@ -0,0 +1,5 @@
+const array=['Arsenal','RealMadrid','Barcelona','Seattle Sounder'];
+array.push('LA Galaxy');
+array.splice(2,1);
+const str=array.join(',')
+console.log(str);
---
+++ Lesson 3/exercise-three.js
@@ -0,0 +1,4 @@
+const array=['Arsenal','RealMadrid','Barcelona','Seattle Sounder'];
+for (let i=0; i<array.length;i++){
+  console.log(i,array[i]);
+}
